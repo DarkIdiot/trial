d4f56d80080b321669f240e14104135899d7fbd1
DarkIdiot <darkidiot@icloud.com>
2017-04-10 00:47:25 +0800

    commit code.

---
+++ curator/src/main/java/com/darkidiot/curator/Constant.java
@@ -0,0 +1,17 @@
+package com.darkidiot.curator;
+
+/**
+ * Copyright (c) for darkidiot
+ * Date:2017/4/10
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * School: CUIT
+ * Desc:
+ */
+public abstract class Constant {
+    final static String ip = "192.168.2.14";
+    final static String port = "2181";
+    final static String splitter = ":";
+    static String getConnetInfo() {
+        return ip+splitter+port;
+    }
+}
---
+++ curator/src/test/java/com/darkidiot/curator/TestCurator.java
@@ -0,0 +1,18 @@
+package com.darkidiot.curator;
+
+import org.junit.Test;
+import static com.darkidiot.curator.Constant.*;
+/**
+ * Copyright (c) for darkidiot
+ * Date:2017/4/10
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * School: CUIT
+ * Desc:
+ */
+public class TestCurator {
+
+    @Test
+    public void testConnect() {
+
+    }
+}
