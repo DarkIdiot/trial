44c879fe0a37f3787ec32ea7a915ec78e2521bc7
DarkIdiot <darkidiot@icloud.com>
2017-04-11 00:05:32 +0800

    commit code.

---
+++ zkClient/readme.md
@@ -0,0 +1,3 @@
+#NOTICE
+    ZkClient是Github上的一个开源的Zookeeper客服端。ZkClient在Zookeeper原生API接口之上进行了包装，是一个更加易用的ZooKeeper客服端。
+  同时,ZkClient在内部实现了诸如Session超时重连、Watcher反复注册等功能。
---
+++ zkClient/src/test/java/com/darkidiot/zkClient/TestZkClient.java
@@ -0,0 +1,18 @@
+package com.darkidiot.zkClient;
+
+import org.junit.Test;
+
+/**
+ * Copyright (c) for darkidiot
+ * Date:2017/4/10
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * School: CUIT
+ * Desc:
+ */
+public class TestZkClient {
+
+    @Test
+    public void testCreateSession() {
+
+    }
+}
