24752c1840b63173aedd7b0c174f8e96ccda69d8
DarkIdiot <darkidiot@icloud.com>
2017-04-11 01:39:06 +0800

    commit code.

--- zkClient/src/test/java/com/darkidiot/zkClient/TestZkClient.java
+++ zkClient/src/test/java/com/darkidiot/zkClient/TestZkClient.java
@@ -7,6 +7,8 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
 
+import java.util.List;
+
 import static com.darkidiot.zkClient.Constant.getConnectInfo;
 
 /**
@@ -34,8 +36,40 @@ public class TestZkClient {
         ZkClient client = new ZkClient(getConnectInfo(), 5000, 5000, new SerializableSerializer());
         log.info("connect ok...");
         Stat stat = new Stat();
-        UserDto user = client.readData("/root-node",stat);
-        log.info("get node data: {}",user.toString());
-        log.info("get stat: {}",stat.toString());
+        UserDto user = client.readData("/root-node", stat);
+        log.info("get node data: {}", user.toString());
+        log.info("get stat: {}", stat.toString());
+    }
+
+    @Test
+    public void testGetChildren() {
+        ZkClient client = new ZkClient(getConnectInfo(), 5000, 5000, new SerializableSerializer());
+        log.info("connect ok...");
+        List<String> children = client.getChildren("/root-node");
+        log.info("get children: {}", children.toString());
+    }
+
+    @Test
+    public void testExists() {
+        ZkClient client = new ZkClient(getConnectInfo(), 5000, 5000, new SerializableSerializer());
+        log.info("connect ok...");
+        boolean exists = client.exists("/root-node");
+        log.info("exists node: {}", exists);
+    }
+
+    @Test
+    public void testDeleteNode() {
+        ZkClient client = new ZkClient(getConnectInfo(), 5000, 5000, new SerializableSerializer());
+        log.info("connect ok...");
+        boolean exists = client.delete("/root-node");
+        log.info("delete node: {}", exists);
+    }
+
+    @Test
+    public void testDeleteRecursiveNode() {
+        ZkClient client = new ZkClient(getConnectInfo(), 5000, 5000, new SerializableSerializer());
+        log.info("connect ok...");
+        boolean exists = client.deleteRecursive("/root-node");
+        log.info("recursive delete node: {}", exists);
     }
 }
