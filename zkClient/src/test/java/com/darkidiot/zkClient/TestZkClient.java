f34afb369780425487a6d9e3efdeb272ff0cc90f
DarkIdiot <darkidiot@icloud.com>
2017-04-11 01:25:03 +0800

    commit code.

--- zkClient/src/test/java/com/darkidiot/zkClient/TestZkClient.java
+++ zkClient/src/test/java/com/darkidiot/zkClient/TestZkClient.java
@@ -1,18 +1,30 @@
 package com.darkidiot.zkClient;
 
+import lombok.extern.slf4j.Slf4j;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.serialize.SerializableSerializer;
+import org.apache.zookeeper.CreateMode;
 import org.junit.Test;
 
+import static com.darkidiot.zkClient.Constant.getConnectInfo;
+
 /**
+ * zkClient测试类
  * Copyright (c) for darkidiot
  * Date:2017/4/10
  * Author: <a href="darkidiot@icloud.com">darkidiot</a>
  * School: CUIT
  * Desc:
  */
+@Slf4j
 public class TestZkClient {
 
     @Test
     public void testCreateSession() {
-
+        ZkClient client = new ZkClient(getConnectInfo(), 5000, 5000, new SerializableSerializer());
+        log.info("connect ok...");
+        UserDto user = new UserDto("idiot", "dark", "man", "java developer", "programming");
+        String path = client.create("/root-node", user, CreateMode.PERSISTENT);
+        log.info("create node: {}", path);
     }
 }
