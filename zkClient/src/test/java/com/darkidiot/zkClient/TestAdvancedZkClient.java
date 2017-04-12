67a8e4fe0db0e8c6d5e0ca6ba02c628c96aca1ff
heqiao <heqiao2@gome.com.cn>
2017-04-12 19:59:58 +0800

    commit.

---
+++ zkClient/src/test/java/com/darkidiot/zkClient/TestAdvancedZkClient.java
@@ -0,0 +1,16 @@
+package com.darkidiot.zkClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * TestAdvancedZkClient测试类
+ * Copyright (c) for darkidiot
+ * Date:2017/4/10
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * School: CUIT
+ * Desc:
+ */
+@Slf4j
+public class TestAdvancedZkClient {
+
+}
