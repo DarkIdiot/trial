938bbe3aa622138f8e146a2f63c487a567092bd0
heqiao <heqiao2@gome.com.cn>
2017-05-25 10:33:31 +0800

    commit code.

---
+++ mongo/src/main/java/com/darkidiot/mongo/dao/UserRepository.java
@@ -0,0 +1,15 @@
+package com.darkidiot.mongo.dao;
+
+import com.darkidiot.mongo.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Copyright (c) for darkidiot
+ * Date:2017/4/11
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * School: CUIT
+ * Desc:
+ */
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+}
