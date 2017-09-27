5f8368854ce1ea1bac30ae070a34c122ee006d58
DarkIdiot <darkidiot@icloud.com>
2017-09-27 21:43:22 +0800

    commit code.

---
+++ lucene/src/test/java/TestIndexWriter.java
@@ -0,0 +1,10 @@
+/**
+ * lucene 测试类
+ * Copyright (c) for darkidiot
+ * Date:2017/5/14
+ * Author: <a href="darkidiot@icloud.com">darkidiot</a>
+ * School: CUIT
+ * Desc:
+ */
+public class TestIndexWriter {
+}
